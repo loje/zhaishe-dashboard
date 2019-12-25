0a43280db8382d11b5dcf4e79e7ae92fb110c081
liujie <loje2012@gmail.com>
2019-12-25 09:49:27 +0800

    fix

--- src/views/Download/index.vue
+++ src/views/Download/index.vue
@@ -25,9 +25,12 @@
         <el-table-column label="下载数"></el-table-column>
         <el-table-column label="链接" prop="link"></el-table-column>
         <el-table-column label="网盘密码" prop="code"></el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="操作" width="180">
           <template slot-scope="scope">
-            <el-button type="warning" @click="edit('编辑', scope.row.objectId)" size="small">编辑</el-button>
+            <el-button-group>
+              <el-button icon="el-icon-edit" @click="edit('编辑', scope.row.objectId)" size="small">编辑</el-button>
+              <el-button icon="el-icon-delete" type="danger" @click="del(scope.row.objectId)" size="small">删除</el-button>
+            </el-button-group>
           </template>
         </el-table-column>
       </el-table>
@@ -70,12 +73,33 @@ export default {
     getlist() {
       this.loading = true;
       let downloadQuery = this.$Bmob.Query('download');
+      downloadQuery.equalTo('notDelete', '==', true);
       if (this.searchText != '') {
         downloadQuery.equalTo('title', '===', this.searchText);
       }
       downloadQuery.find().then((res) => {
         this.loading = false;
         this.tableData = res;
+      });
+    },
+    del(id) {
+      this.$confirm('此操作将删除该资源, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        var downloadQuery = this.$Bmob.Query('download');
+        downloadQuery.set('id', id);
+        downloadQuery.set('notDelete', false);
+        downloadQuery.save().then(() => {
+          this.$message.success('删除成功！');
+          this.getlist();
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });          
       });
     },
   },
